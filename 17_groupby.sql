9ea3e879ba950c0224f698a48d9306425ada64d0
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-08-10 07:57:13 -0500

    Delete 17_groupby.sql

--- 17_groupby.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(age), age FROM users GROUP BY age;
-
-SELECT COUNT(age), age FROM users GROUP BY age ORDER BY age;
-
-SELECT COUNT(age) AS "Cantidad de personas", age FROM users WHERE age > 15 GROUP BY age ORDER BY age;
