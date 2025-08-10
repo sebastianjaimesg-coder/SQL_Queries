e8cf2ba858363080f7990b35ed73756c813da12b
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-08-10 07:56:07 -0500

    Delete 05_like.sql

--- 05_like.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM users WHERE email LIKE "%gmail.com"; -- el % me exclye el contenido antes o despues
-
-SELECT * FROM users WHERE email LIKE "sara%"; --me excluye lo posterior a sara
-
-SELECT * FROM users WHERE email LIKE "%@%"; -- me excluye lo anterior y posterior
