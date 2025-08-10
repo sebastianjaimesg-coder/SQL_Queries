1605b5af0ccbf60b67d364de7bd838344c246404
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-08-10 07:56:01 -0500

    Delete 04_order by.sql

--- 04_order by.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM users ORDER BY age DESC;
-
-SELECT * FROM users ORDER BY age ASC;
-
-SELECT * FROM users ORDER BY age;
-
-SELECT * FROM users WHERE email = "sara@gmail.com" ORDER BY age DESC;
-
-SELECT name FROM users WHERE email = "sara@gmail.com" ORDER BY age DESC;
