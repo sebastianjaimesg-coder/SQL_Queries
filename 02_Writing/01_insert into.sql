2a3b343544d0a222ea853280fed679825bc4d93e
SebastianJaimes <sebastianjaimesg@gmail.com>
2025-08-11 22:10:25 -0500

    Add files via upload

---
+++ 02_Writing/01_insert into.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(user_id, name, surname) VALUES (8, "Maria", "Lopez");
+
+INSERT INTO users(name, surname) VALUES ("Paco", "Perez");
+
+INSERT INTO users(user_id, name, surname) VALUES (11, "El", "Merma");
